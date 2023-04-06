ea34c920290fa161817dc165b9e51d340c9b58d6
Irfan Danial <85051702+IrfanDanial1212@users.noreply.github.com>
2023-04-06 21:49:10 +0800

    Add files via upload

---
+++ StudentRegistration/Script.js
@@ -0,0 +1,12 @@
+
+function validateForm() {
+    // Get form inputs
+    var nameInput = document.getElementById("name");
+    var matricNumberInput = document.getElementById("matricNumber");
+    var emailInput = document.getElementById("email");
+    var currentAddressInput = document.getElementById("currentAddress");
+    var homeAddressInput = document.getElementById("homeAddress");
+    var mobilePhoneNumberInput = document.getElementById("mobilePhoneNumber");
+    var homeNumberInput = document.getElementById("homeNumber");
+    
+    }
